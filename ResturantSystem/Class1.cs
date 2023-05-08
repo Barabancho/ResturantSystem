07ffb4bdefc2fb01fe3eb74061d2c0cd48def770
Rayan <User@DESKTOP-ABNSANK>
2023-05-08 17:08:36 +0300

    proba

---
+++ ResturantSystem/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResturantSystem
+{
+    internal class Class1
+    {
+        WriteLine("Hello world");
+
+    }
+}
