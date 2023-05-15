e02697dbe53adb1a4656d94838f37c087428f7f1
Rayan1 <Student@DESKTOP-5NRPCGA>
2023-05-15 08:59:21 +0300

    promqna 4

--- ResturantSystem/Form4.Designer.cs
+++ ResturantSystem/Form4.Designer.cs
@@ -38,18 +38,20 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 290);
+            this.dataGridView1.Size = new System.Drawing.Size(582, 236);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 399);
+            this.button1.Location = new System.Drawing.Point(9, 324);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(67, 39);
+            this.button1.Size = new System.Drawing.Size(50, 32);
             this.button1.TabIndex = 1;
             this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,31 +59,36 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(85, 399);
+            this.button2.Location = new System.Drawing.Point(64, 324);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(67, 39);
+            this.button2.Size = new System.Drawing.Size(50, 32);
             this.button2.TabIndex = 2;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(158, 399);
+            this.button3.Location = new System.Drawing.Point(118, 324);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(67, 39);
+            this.button3.Size = new System.Drawing.Size(50, 32);
             this.button3.TabIndex = 3;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form4";
             this.Text = "Form4";
             this.Load += new System.EventHandler(this.Form4_Load);
--- ResturantSystem/Form4.cs
+++ ResturantSystem/Form4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace ResturantSystem
 {
     public partial class Form4 : Form
     {
+        private string connectionString = "Data Source=YourServerName;Initial Catalog=YourDatabaseName;Integrated Security=True";
         public Form4()
         {
             InitializeComponent();
@@ -25,8 +27,73 @@ namespace ResturantSystem
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        {/*
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    // Perform your INSERT operation here
+                    // Replace the query and parameter placeholders with your own values
+                    string query = "INSERT INTO YourTableName (column1, column2) VALUES (@value1, @value2)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@value1", "InsertValue1");
+                    command.Parameters.AddWithValue("@value2", "InsertValue2");
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Insert operation successful!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }*/
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {/*
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    // Perform your UPDATE operation here
+                    // Replace the query and parameter placeholders with your own values
+                    string query = "UPDATE YourTableName SET column1 = @value1 WHERE condition";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@value1", "UpdateValue1");
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Update operation successful!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {/*
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Perform your DELETE operation here
+                // Replace the query and parameter placeholders with your own values
+                string query = "DELETE FROM YourTableName WHERE condition";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                MessageBox.Show("Delete operation successful!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }*/
 
         }
     }
+    
 }
